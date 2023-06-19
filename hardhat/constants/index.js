f44fae1190398a79573a1059b2e10795d539ef7e
procodesnet <www.procodes.net@gmail.com>
2023-06-19 15:05:02 +0200

    set METADATA_URL

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x3924d9eC129f59d408d92048ef8F58898EcABe53";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "";
+const METADATA_URL = "https://decentralized-application-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
