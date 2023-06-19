66057e331402a56f1f0e5d1009a543771aab2db5
procodesnet <www.procodes.net@gmail.com>
2023-06-19 14:29:13 +0200

    Initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x3924d9eC129f59d408d92048ef8F58898EcABe53";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
